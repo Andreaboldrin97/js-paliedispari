a6eb34aa2ae2373f7c060f8f75a8406838029ba0
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-06-22 16:03:48 +0200

    create let sum = userChoiseN + pcNumber

--- pariedispari/main.js
+++ pariedispari/main.js
@@ -2,8 +2,9 @@
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 //!apro un form in html
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-//!creo la funzione getNumberRandom
+//!creo la funzione getNrRandom
 // Sommiamo i due numeri
+//!creo la vaiabile sum = userChoiseN + pcNumber
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
 
@@ -24,14 +25,20 @@ function(){
     //cero una var per chiedere la scelta dell'utente
     let userChoise = document.getElementById('user_choise').value ;
     console.log(userChoise);
+
+    //*creo la variabile per l'somma
+    //*visto che il valore di userChoiseN è una stringa la converto in un number per fare la somma
+    let sum =parseInt(userChoiseN)  + pcNumber ;
+    console.log(sum)
 })
+
 
 //!invoco la funzione con i numeri da 1 a 5
 //! e la associo a una variabile
 let pcNumber = getNRandom(1,5);
-console.log(pcNumber)
+console.log(pcNumber);
 
 //? creo la funzione per generare un numero random
 function getNRandom(min, max) {
-    return Math.floor(Math.random() * (max - min) + min)
+    return Math.floor(Math.random() * (max - min) + min);
   }
