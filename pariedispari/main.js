708c14332e519396b8a2fd20ea2aec5b977f484f
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-06-22 16:31:13 +0200

    edit id choise html

--- pariedispari/main.js
+++ pariedispari/main.js
@@ -21,23 +21,31 @@ let sum ;
 btnSend.addEventListener(`click`, 
 function(){
     //cero una var per chiedere il numero dell'utente
-    let userChoiseN= document.getElementById('user_choise_N').value ;
-    console.log(userChoiseN);
+    let userChoiceN= document.getElementById('user_choice_N').value ;
+    console.log(userChoiceN);
 
     //cero una var per chiedere la scelta dell'utente
-    let userChoise = document.getElementById('user_choise').value ;
-    console.log(userChoise);
+    let userChoice = document.getElementById('user_choice').value ;
+    console.log(userChoice);
 
     //*creo la variabile per l'somma
     //*visto che il valore di userChoiseN è una stringa la converto in un number per fare la somma
-    let sum =parseInt(userChoiseN)  + pcNumber ;
+    let sum =parseInt(userChoiceN)  + pcNumber ;
     console.log(sum)
+
+    //?creo un if per vedere se la somma del numero è pari o dispari
+    if((sum % 2 === 0) && (userChoice.value === 'Pari')){
+        console.log(`bravo hai fatto bene ha scegliere Pari`);
+        }else if((sum % 2 === 1) && (userChoice.value === 'Dispari')){
+        console.log(`bravo hai fatto bene ha scegliere Dispari`);
+        }else{
+            console.log(`hey amico mi dispiace ma ho vinto io`)
+        }
+
 })
 
-//?creo un if per vedere se la somma del numero è pari o dispari
-if(sum % 2 === 0) && (userChoise.value === value='pari'){
-
-}
+  
+   
 
 //!invoco la funzione con i numeri da 1 a 5
 //! e la associo a una variabile
